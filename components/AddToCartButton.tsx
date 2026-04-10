80ce2e290dec911a4f1d61e7aac802a2e014b79e
askarizhan-del <askar.izhan@gmail.com>
2026-04-10 15:45:49 +0500

    Replace AddToCartButton functionality with a test button

--- components/AddToCartButton.tsx
+++ components/AddToCartButton.tsx
@@ -1,19 +1,7 @@
-'use client'
-
-import { useCart } from '@/components/CartContext'
-
-export default function AddToCartButton({ product }) {
-  const { addToCart } = useCart()
-
-  return (
-    <button
-      onClick={() => {
-        console.log('CLICK WORKS')
-        addToCart(product)
-      }}
-      className="w-full py-4 bg-green-700 text-white font-bold rounded-xl"
-    >
-      🛒 В корзину
-    </button>
-  )
-}
+<button
+  onClick={() => {
+    alert('CLICK')
+  }}
+>
+  TEST
+</button>
